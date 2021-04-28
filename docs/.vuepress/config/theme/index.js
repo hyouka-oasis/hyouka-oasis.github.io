289d300ccb8b2c054f9aeaa2c6e67bac9bf5b64a
HyoukaM <838611931@qq.com>
2021-04-28 23:16:20 +0800

    feat(twosum): 更新算法

--- docs/.vuepress/config/theme/index.js
+++ docs/.vuepress/config/theme/index.js
@@ -8,20 +8,20 @@ module.exports = Object.assign({}, themeReco, {
     search: true,
     searchMaxSuggestions: 10,
     // 自动形成侧边导航
-    sidebar: 'auto',
+    subSidebar: 'auto',
     valineConfig: {
         appId: 'FdT4yPiuI8yl0Q1ndeKy0Dr4-gzGzoHsz',// your appId
         appKey: '3uFvPvYeuHfUzULmU7ntRazw', // your appKey，
         visitor: true,
         lang: 'zh-CN',
         placeholder: '请填写你的评论吧😊😊😊'
-    }
-    // friendLink: [
-    //   {
-    //     title: '俊劫',
-    //     desc: '一个非常能收集杂七杂八资源的人，想学点其他的可以先去他的个人博客里面看看',
-    //     logo: "https://alexwjj.github.io/logo.png",
-    //     link: 'https://alexwjj.github.io'
-    //   }
-    // ],
+    },
+    friendLink: [
+      {
+        title: '俊劫',
+        desc: '一个非常能收集杂七杂八资源的人，想学点其他的可以先去他的个人博客里面看看',
+        logo: "https://alexwjj.github.io/logo.png",
+        link: 'https://alexwjj.github.io'
+      }
+    ],
 })
